42264536371a053f784c818812f357b26094d4e2
TasiDora <158039817+TasiDora@users.noreply.github.com>
2024-06-03 13:29:31 +0200

    Add files via upload

---
+++ tisza-to Tasi Dóra/tiszato.js
@@ -0,0 +1,18 @@
+function turizmus(melyik){
+    document.getElementById('turizmusszovegkeret').style.display='none';
+    document.getElementById('turizmuskep').src= 'kepek/' + melyik + '.jpg';
+    document.getElementById('turizmuskepkeret').style.display='block';   
+}
+
+function keprejt(melyik){
+  document.getElementById('turizmuskepkeret').style.display='none';
+  document.getElementById('turizmusszovegkeret').style.display='none';    
+}
+
+
+
+
+
+
+
+
